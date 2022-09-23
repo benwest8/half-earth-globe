6a85b11514883c41854c19f2cfacc07fab473739
Ben West <benwest@Bens-MacBook-Air.local>
2022-09-22 19:47:18 -0600

    Add a data uploader

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ require([
   "esri/Basemap",
   "esri/layers/FeatureLayer",
   "esri/widgets/LayerList",
+  "esri/request",
+  "esri/Graphic",
   "dojo/domReady!" // will not be called until DOM is ready
   ], function (
   Map,
@@ -12,7 +14,9 @@ require([
   TileLayer,
   Basemap,
   FeatureLayer,
-  LayerList
+  LayerList,
+  request,
+  Graphic
   ) {
     
     const satelliteLayer = new TileLayer({
@@ -67,4 +71,57 @@ require([
       view.ui.add(layerList, {
         position: "top-right"
       });
+
+      const uploadForm = document.getElementById("uploadForm");
+
+      uploadForm.addEventListener("change", function (event) {
+      const filePath = event.target.value.toLowerCase();
+      //only accept .zip files
+      if (filePath.indexOf(".zip") !== -1) {
+        generateFeatureCollection(uploadForm)
+      } 
+    });
+
+    function generateFeatureCollection(uploadFormNode) {
+      const generateRequestParams = {
+        filetype: "shapefile",
+          publishParameters: JSON.stringify({
+          targetSR: view.spatialReference
+        }),
+        f: "json"
+      };
+        request("https://www.arcgis.com/sharing/rest/content/features/generate", {
+        query: generateRequestParams,
+        body: uploadFormNode,
+        responseType: "json"
+      }).then(function (response) {
+        addShapefileToMap(response.data.featureCollection);
+        console.log(response)
+        })
+    }
+    function createFeaturesGraphics(layer) {
+      console.log(layer)
+      return layer.featureSet.features.map(function (feature) {
+        return Graphic.fromJSON(feature);
+      });
+    }
+    function createFeatureLayerFromGraphic(graphics) {
+      return new FeatureLayer({
+        objectIdField: "FID",
+        source: graphics,
+        title: 'User uploaded shapefile'
+      });
+    }
+    function addShapefileToMap(featureCollection) {
+      let sourceGraphics = [];
+      const collectionLayers = featureCollection.layers;
+      const mapLayers = collectionLayers.map(function (layer) {
+        const graphics = createFeaturesGraphics(layer);
+        sourceGraphics = sourceGraphics.concat(graphics);
+        const featureLayer = createFeatureLayerFromGraphic(graphics)
+        return featureLayer;
+      });
+      map.addMany(mapLayers);
+      view.goTo({target: sourceGraphics, tilt: 40});
+    }
   });
