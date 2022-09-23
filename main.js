ff96c102342c26e5f9b76caf65021aa7d980e43f
Ben West <benwest@Bens-MacBook-Air.local>
2022-09-22 18:29:37 -0600

    add custom basemap

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 require([
   "esri/Map",
   "esri/views/SceneView",
+  "esri/layers/TileLayer",
+  "esri/Basemap",
   "dojo/domReady!" // will not be called until DOM is ready
   ], function (
   Map,
-  SceneView
+  SceneView,
+  TileLayer,
+  Basemap
   ) {
     
+    const satelliteLayer = new TileLayer({
+      url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
+      title: "satellite"
+    })
+
+    const fireflyLayer = new TileLayer({
+      url: "https://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/HalfEarthFirefly/MapServer",
+      title: "half-earth-firefly"
+    })
+
+    const basemap = new Basemap({
+      baseLayers: [satelliteLayer, fireflyLayer],
+      title: "half-earth-basemap",
+      id: "half-earth-basemap"
+    });
+    
     const map = new Map({
-      basemap: 'satellite'
+      basemap: basemap
     });
   
     const view = new SceneView({
